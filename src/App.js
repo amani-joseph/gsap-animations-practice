2913396d1debd2bb3ff51b1d4956353f913a0e85
amani-joseph <mr.amanijoseph@gmail.com>
2022-05-25 14:40:46 +0300

    Bugged

--- src/App.js
+++ src/App.js
@@ -1,18 +1,44 @@
 /** @format */
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect, useState } from "react";
 import "./App.css";
-import { TweenMax, Power3 } from "gsap";
+import { TweenMax, gsap, Power3 } from "gsap";
 
 function App() {
-	let circleYellow = useRef(null);
+	let circle = useRef(null);
 	let circleRed = useRef(null);
 	let circleBlue = useRef(null);
 
-	useEffect(() => {
-		TweenMax.from(circleYellow, 0.7, {
-			opacity: 0,
-			x: 40,
+	const [state, setState] = useState[false];
+
+	const handleExpand = () => {
+		TweenMax.to(circleRed, 0.8, {
+			width: 200,
+			height: 200,
+			opacity: 0.2,
+			x: 200,
 			ease: Power3.easeOut,
+			delay: 0.2,
+		});
+		setState(true);
+	};
+
+	useEffect(() => {
+		TweenMax.from(circle, 0.8, {
+			opacity: 0.2,
+			x: 200,
+			ease: Power3.easeOut,
+		});
+		TweenMax.from(circleRed, 0.8, {
+			opacity: 0.2,
+			x: 200,
+			ease: Power3.easeOut,
+			delay: 0.2,
+		});
+		TweenMax.from(circleBlue, 0.8, {
+			opacity: 0.2,
+			x: 200,
+			ease: Power3.easeOut,
+			delay: 0.4,
 		});
 	}, []);
 	return (
@@ -21,7 +47,7 @@ function App() {
 				<div className="circle-container">
 					<div
 						ref={(el) => {
-							circleYellow = el;
+							circle = el;
 						}}
 						className="circle"></div>
 					<div
