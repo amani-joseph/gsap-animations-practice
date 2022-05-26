1248c26d29cabf99a5f6d7ca2776651f67ad61dc
amani-joseph <mr.amanijoseph@gmail.com>
2022-05-26 13:04:00 +0300

    Basic structure

--- src/App.js
+++ src/App.js
@@ -1,46 +1,121 @@
 /** @format */
 
-import React, { useRef, useEffect } from "react";
+import React from "react";
+
+// Arrows
+import ArrowLeft from "./assets/arrow-left.svg";
+import ArrowRight from "./assets/arrow-right.svg";
+
+//
+
+// import "reset-css";
 import "./App.scss";
-import People from "./images/people.webp";
-import { gsap, Power2 } from "gsap";
-import CSSRulePlugin from "gsap/CSSRulePlugin";
 
-const App = () => {
-	let image = useRef(null);
-	let container = useRef(null);
-	let imageReveal = CSSRulePlugin.getRule(".img-container:after");
+const testimonials = [
+	{
+		name: "Julia Cameron",
+		title: "Creative Director, VISA",
+		image: `${require("./assets/image3.jpg")}`,
+		quote: "It's all good. I was amazed at the quality of the Design. We've seen amazing results already.",
+	},
+	{
+		name: "Mark Jacobs",
+		title: "Tech Lead, Google",
+		image: `${require("./assets/image.jpg")}`,
+		quote: "The rebranding has really helped our business. Definitely worth the investment.",
+	},
+	{
+		name: "Lisa Bearings",
+		title: "Brand Coordinator, Facebook",
+		image: `${require("./assets/image2.jpg")}`,
+		quote: "The service was excellent. Absolutely wonderful! A complete redesign did it for us.",
+	},
+];
 
-	let tl = gsap.timeline();
-
-	useEffect(() => {
-		tl.to(container, 0, { css: { visibility: "visible" } });
-		tl.to(imageReveal, 1.4, { width: "0%", ease: Power2.easeInOut });
-		tl.from(image, 1, {
-			scale: 1.6,
-			ease: Power2.easeInOut,
-			delay: -1.4,
-		});
-	}, []);
-
+function App() {
 	return (
-		<section className="main">
-			<p>GSAP IMAGE REVEAL</p>
-			<div className="container" ref={(el) => (container = el)}>
-				<>
-					<div className="img-container">
-						<img
-							ref={(el) => {
-								image = el;
-							}}
-							src={People}
-							alt="People"
-						/>
+		<div className="testimonial-section">
+			<div className="testimonial-container">
+				<div className="arrows left">
+					<span>
+						<img src={ArrowLeft} alt="Left arrow" />
+					</span>
+				</div>
+				<div className="inner">
+					<div className="t-image">
+						<ul>
+							<li>
+								<img
+									src={testimonials[0].image}
+									alt={testimonials[0].name + " image"}
+								/>
+							</li>
+							<li>
+								<img
+									src={testimonials[1].image}
+									alt={testimonials[1].name + " image"}
+								/>
+							</li>
+							<li>
+								<img
+									src={testimonials[2].image}
+									alt={testimonials[2].name + " image"}
+								/>
+							</li>
+						</ul>
 					</div>
-				</>
+					<div className="t-content">
+						<ul>
+							<li>
+								<div className="content-inner ">
+									<p className="quote">
+										{testimonials[0].quote}
+									</p>
+									<h3 className="name">
+										{testimonials[0].name}
+									</h3>
+									<h4 className="title">
+										{testimonials[0].title}
+									</h4>
+								</div>
+							</li>
+							<li>
+								<div className="content-inner ">
+									<p className="quote">
+										{testimonials[0].quote}
+									</p>
+									<h3 className="name">
+										{testimonials[0].name}
+									</h3>
+									<h4 className="title">
+										{testimonials[0].title}
+									</h4>
+								</div>
+							</li>
+							<li>
+								<div className="content-inner ">
+									<p className="quote">
+										{testimonials[0].quote}
+									</p>
+									<h3 className="name">
+										{testimonials[0].name}
+									</h3>
+									<h4 className="title">
+										{testimonials[0].title}
+									</h4>
+								</div>
+							</li>
+						</ul>
+					</div>
+				</div>
+				<div className="arrows left">
+					<span>
+						<img src={ArrowRight} alt="Right arrow" />
+					</span>
+				</div>
 			</div>
-		</section>
+		</div>
 	);
-};
+}
 
 export default App;
